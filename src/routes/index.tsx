12787529574bd3b2452efc17a21c9bbeb80f8813
Khaled FERJANI <kferjani@linagora.com>
2022-12-03 01:57:57 +0100

    adjust titles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ export default component$(() => {
     <div class={'w-full'}>
       <div class={'flex flex-row'}>
         <h1 class='flex-grow mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl'>
-          Using Qwik
+          Qwik
         </h1>
         <Link
           href='/react'
